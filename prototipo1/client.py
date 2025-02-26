e34b21cbcee67c267adcf24aab50b920ca21d829
Veronica Perez <s.veronica.pr@hotmail.com>
2025-02-26 13:28:59 +0100

    ds

--- prototipo1/client.py
+++ prototipo1/client.py
@@ -11,19 +11,22 @@ class User:
         return f"Id: {self.id}, Username: {self.username}, Password: {self.password}, Email: {self.email}"
 
 class UserDAO:
+    @staticmethod
     def get_user_by_username(username):
-        response = requests.get(f'http://localhost:10050/prototipo1/getuser?username={username}')
+        response = requests.get(f'http://localhost:5000/user/{username}')  # Update to match Flask server
         if response.status_code == 200:
             user_data = response.json()
-            user = User(user_data['id'], user_data['username'], user_data['password'], user_data['email'])
+            user = User(user_data['id'], user_data['username'], '', user_data['email'])
             return user
         else:
             return None
-        
+
 class ViewConsole:
+    @staticmethod
     def getInputUsername():
         return input("Enter username: ")
     
+    @staticmethod
     def showUserInfo(username):
         user = UserDAO.get_user_by_username(username)
         if user:
